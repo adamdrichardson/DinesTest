af36b7d876379c939a41c533f23f097ccd488aa9
Adam Richardson <a_richardson@me.com>
2023-02-17 15:47:59 +0000

    Added UI Tests
    Added and executed UI tests
    
    Also fixed a typo in the Checkout button text that was picked up due to the UI test failing

--- DinesTestUITests/DinesTestUITests.swift
+++ DinesTestUITests/DinesTestUITests.swift
@@ -22,21 +22,131 @@ class DinesTestUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testRootMenuDrink() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        let optionRequired = "Drinks"
+        
+        let drinkLabel = app.staticTexts[optionRequired]
+        
+        app.swipeUp()
+        
+        XCTAssertTrue(drinkLabel.isHittable)
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testRootMenuFood() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        let optionRequired = "Food"
+        
+        let foodLabel = app.staticTexts[optionRequired]
+        
+        app.swipeUp()
+        app.swipeDown()
+        
+        XCTAssertTrue(foodLabel.isHittable)
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testRootMenuDessert() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        let optionRequired = "Desserts"
+        
+        let dessertLabel = app.staticTexts[optionRequired]
+        
+        app.swipeUp()
+        
+        XCTAssertTrue(dessertLabel.isHittable)
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testBasketTab() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let basket = app.buttons["Basket"]
+        
+        XCTAssertTrue(basket.isHittable)
+    }
+    
+    func testMenuTab() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let menu = app.buttons["Basket"]
+        
+        XCTAssertTrue(menu.isHittable)
+    }
+    
+    func testOrderTab() throws {
+        let app = XCUIApplication()
+        app.launch()
+                
+        let order = app.buttons["Order History"]
+        
+        XCTAssertTrue(order.isHittable)
+    }
+    
+    func testOrderTitle() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let orderButton = app.buttons["Order History"]
+        orderButton.tap()
+        
+        let orderTitle = app.staticTexts["Order History"]
+        
+        XCTAssertTrue(orderTitle.isHittable)
+    }
+    
+    func testEmptyOrders() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let orderButton = app.buttons["Order History"]
+        orderButton.tap()
+        
+        let emptyOrderText = app.staticTexts["There are no previous orders."]
+        
+        XCTAssertTrue(emptyOrderText.isHittable)
+    }
+    
+    func testEmptyBasket() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let basketButton = app.buttons["Basket"]
+        basketButton.tap()
+        
+        let emptyBasketText = app.staticTexts["There are no items in your basket."]
+        
+        XCTAssertTrue(emptyBasketText.isHittable)
+    }
+    
+    func testCheckoutButton() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let basketButton = app.buttons["Basket"]
+        basketButton.tap()
+        
+        let checkoutButton = app.buttons["Checkout"]
+        
+        XCTAssertTrue(checkoutButton.isHittable)
+    }
+
 }
